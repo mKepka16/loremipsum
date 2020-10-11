df87406d79bef1291db614f7fb7a86377ec831cf
Adam <adam.kot511@gmail.com>
2020-10-11 23:26:59 +0200

    added login section without logic & functionality

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import "./login.css";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import PersonIcon from "@material-ui/icons/Person";
+
+class Login extends React.Component {
+  validateEmail = (email, result) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    result = re.test(String(email).toLowerCase());
+
+    return !result;
+  };
+
+  render() {
+    return (
+      <div>
+        <Grid container className="login" alignItems="center">
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <Grid item xs={12}>
+                  <CardHeader
+                    title={
+                      <Typography
+                        align="center"
+                        variant="h5"
+                        color="textSecondary"
+                      >
+                        <PersonIcon fontSize="inherit" />
+                        Zaloguj się
+                      </Typography>
+                    }
+                  ></CardHeader>
+
+                  <Divider light />
+                  <CardContent>
+                    <Typography align="center">
+                      <TextField
+                        id="outlined-error"
+                        label="Adress email"
+                        type="email"
+                        variant="outlined"
+                        error="true"
+                        value="true"
+                        helperText={"Podaj swój adres email"}
+                      />
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography align="center">
+                      <TextField
+                        id="outlined-error"
+                        label="Hasło"
+                        type="password"
+                        variant="outlined"
+                        helperText={"Podaj swoje hasło"}
+                      />
+                    </Typography>
+                  </CardContent>
+                </Grid>
+              </CardContent>
+              <CardActions>
+                <Grid item xs={6}>
+                  <Typography align="right">
+                    <Button variant="contained" color="primary">
+                      Zarejestruj
+                    </Button>
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography align="left">
+                    <Button variant="contained" color="primary">
+                      Zaloguj się
+                    </Button>
+                  </Typography>
+                </Grid>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Login;
