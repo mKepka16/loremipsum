3af12415ec592c9223eff83f2afb61b02846219e
Michał Kępka <michal.kepka16@gmail.com>
2020-10-19 20:04:19 +0200

    Fixed some problems

--- client/src/components/Section-Main/Data/main-page-data.js
+++ client/src/components/Section-Main/Data/main-page-data.js
@@ -1,10 +1,55 @@
 const data = {
+    1: {
+        photo: '1mies.svg',
+        childInfo: 'W jajowodzie dochodzi do zapłodnienia. Zygota wędruje przez jajowód do macicy. Następuje szybki podział zapłodnionej komórki. Siódmego dnia jest ich około 100. Zarodek zagnieżdża się w śluzówce macicy.',
+        symptoms: 'Jeszcze nie ma',
+    },
+    2: {
+        photo: '2mies.svg',
+        childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
+        symptoms: 'Też pusto bo uciekam na pole',
+    },
     3: {
         photo: '1mies.svg',
         childInfo: 'W jajowodzie dochodzi do zapłodnienia. Zygota wędruje przez jajowód do macicy. Następuje szybki podział zapłodnionej komórki. Siódmego dnia jest ich około 100. Zarodek zagnieżdża się w śluzówce macicy.',
         symptoms: 'Jeszcze nie ma',
     },
     4: {
+        photo: '2mies.svg',
+        childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
+        symptoms: 'Też pusto bo uciekam na pole',
+    },
+    5: {
+        photo: '1mies.svg',
+        childInfo: 'W jajowodzie dochodzi do zapłodnienia. Zygota wędruje przez jajowód do macicy. Następuje szybki podział zapłodnionej komórki. Siódmego dnia jest ich około 100. Zarodek zagnieżdża się w śluzówce macicy.',
+        symptoms: 'Jeszcze nie ma',
+    },
+    6: {
+        photo: '2mies.svg',
+        childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
+        symptoms: 'Też pusto bo uciekam na pole',
+    },
+    7: {
+        photo: '1mies.svg',
+        childInfo: 'W jajowodzie dochodzi do zapłodnienia. Zygota wędruje przez jajowód do macicy. Następuje szybki podział zapłodnionej komórki. Siódmego dnia jest ich około 100. Zarodek zagnieżdża się w śluzówce macicy.',
+        symptoms: 'Jeszcze nie ma',
+    },
+    8: {
+        photo: '2mies.svg',
+        childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
+        symptoms: 'Też pusto bo uciekam na pole',
+    },
+    9: {
+        photo: '2mies.svg',
+        childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
+        symptoms: 'Też pusto bo uciekam na pole',
+    },
+    10: {
+        photo: '1mies.svg',
+        childInfo: 'W jajowodzie dochodzi do zapłodnienia. Zygota wędruje przez jajowód do macicy. Następuje szybki podział zapłodnionej komórki. Siódmego dnia jest ich około 100. Zarodek zagnieżdża się w śluzówce macicy.',
+        symptoms: 'Jeszcze nie ma',
+    },
+    11: {
         photo: '2mies.svg',
         childInfo: 'Zarodek składa się z kilkuset komórek. Układają się one w trzy grupy tzw. listki zarodkowe. Z każdego powstają później różne narządy dziecka. Z pierwszej grupy formuje się układ nerwowy i skóra, z drugiej narządy układu pokarmowego a z trzeciej mięśnie, kości i układ krwionośny. W tym tygodniu tworzy się też łożysko. Zarodek ma około 1mm długości i jest półprzezroczysty.',
         symptoms: 'Też pusto bo uciekam na pole',
